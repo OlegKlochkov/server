84389f5c4cf8e8b217e1bd12a73274fa1d2c6437
Olekoster <olegk1576@yandex.ru>
2021-12-14 00:12:00 +0300

    added get_products

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require('./routes/cors')
 app.use(cors)
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-app.use('/api/auth', authRouter)
+app.use('', authRouter)
 
 const start = () => {
     try {
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -17,16 +17,16 @@ router.post('/registration',
             if (!errors.isEmpty()) {
                 return (res.status(400).json({ message: 'Uncorrect request', errors }))
             }
-            const { email, password, name } = req.body
+            const { email, password, name, second_name } = req.body
             const hashPass = await bcrypt.hash(password, 5);
-            mysqls.executeQuery(`SELECT * FROM users WHERE email = '${email}' LIMIT 1`, function (err, rows, fields) {
+            mysqls.executeQuery(`SELECT * FROM clients WHERE email = '${email}' LIMIT 1`, function (err, rows, fields) {
                 if (err) {
                     console.log('[DATABASE | ERROR] ' + err);
                     return;
                 }
 
                 if (rows.length === 0) {
-                    let sql = "INSERT INTO users (email, name, password) VALUES ('" + email + "', '" + name + "', '" + hashPass + "')";
+                    let sql = `INSERT INTO clients (email, name, second_name, password) VALUES ('${email}', '${name}', '${second_name}', '${hashPass}')`;
                     mysqls.executeQuery(sql)
                     res.send("Вы успешно зарегестрировались.")
                 } else if (rows[0].email === email) {
@@ -44,7 +44,7 @@ router.post('/login', async (req, res) => {
     try {
         const { email, password } = req.body
 
-        mysqls.executeQuery(`SELECT * FROM users WHERE email = '${email}' LIMIT 1`, function (err, rows, fields) {
+        mysqls.executeQuery(`SELECT * FROM clients WHERE email = '${email}' LIMIT 1`, function (err, rows, fields) {
             if (err) {
                 console.log('[DATABASE | ERROR] ' + err);
                 return;
@@ -58,11 +58,11 @@ router.post('/login', async (req, res) => {
                 return res.status(400).json({ message: "Неправильный пароль или логин." })
             }
 
-            const token = jwt.sign({ id: rows[0].id }, config.get("secret_key"), { expiresIn: "1h" })
+            const token = jwt.sign({ id: rows[0].client_id }, config.get("secret_key"), { expiresIn: "1h" })
             return res.json({
                 token,
                 user: {
-                    id: rows[0].id,
+                    id: rows[0].client_id,
                     email: rows[0].email
                 }
             })
@@ -75,7 +75,7 @@ router.post('/login', async (req, res) => {
 
 router.get('/auth', authMidleware ,async (req, res) => {
     try {
-        mysqls.executeQuery(`SELECT * FROM users WHERE id = '${req.user.id}' LIMIT 1`, function (err, rows, fields) {
+        mysqls.executeQuery(`SELECT * FROM clients WHERE client_id = '${req.user.id}' LIMIT 1`, function (err, rows, fields) {
             if (err) {
                 console.log('[DATABASE | ERROR] ' + err);
                 return;
@@ -83,13 +83,36 @@ router.get('/auth', authMidleware ,async (req, res) => {
             if (rows.length === 0) {
                 return res.status(400).json({ message: "Пользователь не найден." })
             }
-            const token = jwt.sign({ id: rows[0].id }, config.get("secret_key"), { expiresIn: "1h" })
+            const token = jwt.sign({ id: rows[0].client_id }, config.get("secret_key"), { expiresIn: "1h" })
             return res.json({
                 token,
                 user: {
-                    id: rows[0].id,
+                    id: rows[0].client_id,
                     email: rows[0].email
                 }
+            })
+        });
+    } catch (e) {
+        console.log(e);
+        res.send({ message: "server error" })
+    }
+})
+
+router.post('/get_products', async (req, res) => {
+    try {
+        mysqls.executeQuery(`SELECT * FROM products`, function (err, rows, fields) {
+
+            if (err) {
+                console.log('[DATABASE | ERROR] ' + err);
+                return;
+            }
+
+            if (rows.length === 0) {
+                return res.status(400).json({ message: "Пользователь не найден." })
+            }
+
+            return res.json({
+                products: rows
             })
         });
     } catch (e) {
